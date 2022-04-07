81c1ec590306709975f9c18563dcee6baa54a06f
DmitrySinyapkin <dsinyapkin@mail.ru>
2022-04-07 18:52:05 +0300

    added some functions for api

---
+++ next-store/api/fakeStoreApi.ts
@@ -0,0 +1,20 @@
+import { CATEGORIES_URL, PRODUCTS_URL } from "../constants/fakeStoreApi"
+
+const doGetRequest = async (url: string) => {
+    const res = await fetch(url)
+    const data = await res.json()
+    return data
+}
+
+export const getCategories = () => {
+    return doGetRequest(CATEGORIES_URL)
+}
+
+export const getProductsByCategory = (category: string) => {
+    const url = CATEGORIES_URL + `/${category}`
+    return doGetRequest(url)
+}
+
+export const getAllProducts = () => {
+    return doGetRequest(PRODUCTS_URL)
+}
