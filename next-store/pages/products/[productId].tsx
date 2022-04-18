a730d0fbbc7c07a1ee9a6788b2ef9bcf059e68d3
DmitrySinyapkin <dsinyapkin@mail.ru>
2022-04-18 17:47:54 +0300

    added modal success message

--- next-store/pages/products/[productId].tsx
+++ next-store/pages/products/[productId].tsx
@@ -6,6 +6,7 @@ import { firstCharToUpperCase } from "../../utils/firstCharToUpperCase";
 import styles from "../../styles/Product.module.scss"
 import { ProductInCartType } from "../../types/cart";
 import { useCountContext } from "../../context/CountContext";
+import { Space, message } from "antd";
 
 const Product: NextPage = ({ categories, product }: InferGetServerSidePropsType<GetServerSideProps>) => {
     const [count, setCount] = useCountContext()
@@ -19,24 +20,27 @@ const Product: NextPage = ({ categories, product }: InferGetServerSidePropsType<
         const changed = [...products, newProduct]
         localStorage.setItem('mnstore-cart', JSON.stringify(changed))
         setCount((prev: number) => prev + 1)
+        message.success('Added to cart')
     }
 
     return (
         <MainLayout categories={categories} title={firstCharToUpperCase(product.title)} description={product.description}>
-            <div className={styles.product}>
-                <h1>{product.title}</h1>
-                <div className={styles.container}>
-                    <figure>
-                        <Image src={product.image} alt={product.title} layout="fill" objectFit="contain" />
-                    </figure>
-                    <div className={styles.buy_block}>
-                        <div className={styles.price}>{product.price} $</div>
-                        <button onClick={() => handleAddToCart(product)}>Add to cart</button>
+                <div className={styles.product}>
+                    <h1>{product.title}</h1>
+                    <div className={styles.container}>
+                        <figure>
+                            <Image src={product.image} alt={product.title} layout="fill" objectFit="contain" />
+                        </figure>
+                        <div className={styles.buy_block}>
+                            <div className={styles.price}>{product.price} $</div>
+                            <Space>
+                                <button onClick={() => handleAddToCart(product)}>Add to cart</button>
+                            </Space>
+                        </div>
                     </div>
+                    <div className={styles.description}>{product.description}</div>
+                    <div className={styles.rating}>Rating: {product.rating.rate}</div>
                 </div>
-                <div className={styles.description}>{product.description}</div>
-                <div className={styles.rating}>Rating: {product.rating.rate}</div>
-            </div>
         </MainLayout>
     )
 }
