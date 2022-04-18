f104f7035acb8dedb44f5ba6b058c303cddf69b1
DmitrySinyapkin <dsinyapkin@mail.ru>
2022-04-18 18:57:11 +0300

    footer added

--- next-store/layouts/MainLayout.tsx
+++ next-store/layouts/MainLayout.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from "react";
 import Sidebar from "../components/Sidebar";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/MainLayout.module.scss"
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const MainLayout = ({children, categories, title, description}: {children: ReactNode, categories: string[], title: string, description: string}) => {
     return (
@@ -19,16 +19,7 @@ const MainLayout = ({children, categories, title, description}: {children: React
             <aside><Sidebar categories={categories} /></aside>
             <main>{children}</main>
             <footer>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by{' '}
-                    <span className={styles.logo}>
-                        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                    </span>
-                </a>
+               <Footer />
             </footer>
         </div>
     )
