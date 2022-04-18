c99b4a82bfb52245a6722e4609f70e48cee519f2
DmitrySinyapkin <dsinyapkin@mail.ru>
2022-04-18 15:49:13 +0300

    visual improvement

--- next-store/components/CartItem.tsx
+++ next-store/components/CartItem.tsx
@@ -2,6 +2,7 @@ import { InputNumber } from "antd"
 import Image from "next/image"
 import { ProductInCartType } from "../types/cart"
 import styles from "../styles/CartItem.module.scss"
+import Link from "next/link"
 
 const CartItem = ({ product, handleQuantityChange }: { product: ProductInCartType, handleQuantityChange: Function}) => {
 
@@ -11,15 +12,23 @@ const CartItem = ({ product, handleQuantityChange }: { product: ProductInCartTyp
 
     return (
         <div className={styles.item}>
-            <figure>
-                <Image src={product.image} alt={product.title} layout="fill" objectFit="contain" />
-            </figure>
-            <div>{product.title}</div>
+            <div className={styles.card}>
+                <Link href={`/products/${product.id}`}>
+                    <a>
+                        <div className={styles.description}>
+                            <figure>
+                                <Image src={product.image} alt={product.title} layout="fill" objectFit="contain" />
+                            </figure>
+                            <div>{product.title}</div>
+                        </div>
+                    </a>
+                </Link> 
+            </div>
             <div className={styles.quantity}>
-                <div>Quantity</div>
+                <div>Quantity:</div>
                 <InputNumber value={product.quantity} onChange={handleChange} />
             </div>
-            <div>{product.quantity * +product.price} $</div>
+            <div className={styles.price}>{product.quantity * +product.price} $</div>
             <button>Remove</button>
         </div>
     )
