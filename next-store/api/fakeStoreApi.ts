3eddc0ee00a9b7eec34a554c244ed1699f842e55
DmitrySinyapkin <dsinyapkin@mail.ru>
2022-04-20 19:11:36 +0300

    added login method

--- next-store/api/fakeStoreApi.ts
+++ next-store/api/fakeStoreApi.ts
@@ -1,7 +1,20 @@
-import { CATEGORIES_URL, PRODUCTS_URL } from "../constants/fakeStoreApi"
+import { CATEGORIES_URL, LOGIN_URL, POST_METHOD, PRODUCTS_URL } from "../constants/fakeStoreApi"
 
 const doGetRequest = async (url: string) => {
     const res = await fetch(url)
+    const data = await res.json()
+    return data
+}
+
+const doPostRequest = async (url: string, body: BodyInit | null | undefined) => {
+    const options = {
+        method: POST_METHOD,
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        }),
+        body: body
+    }
+    const res = await fetch(url, options)
     const data = await res.json()
     return data
 }
@@ -22,4 +35,15 @@ export const getAllProducts = () => {
 export const getProductById = (id: string | string[] | undefined) => {
     const url = PRODUCTS_URL + `/${id}`
     return doGetRequest(url)
+}
+
+export const login = (username: string, password: string) => {
+    const url = LOGIN_URL
+    const body = JSON.stringify({
+        username,
+        password
+    })
+    doPostRequest(url, body)
+        .then(resp => console.log(resp))
+        .catch(err => console.log(err))
 }
