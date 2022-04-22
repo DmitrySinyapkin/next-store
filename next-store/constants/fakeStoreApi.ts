f27d8db6cdee9cc0c6dc5e811152a8d8a07200ca
DmitrySinyapkin <dsinyapkin@mail.ru>
2022-04-22 22:49:33 +0300

    order confirmation page added

--- next-store/constants/fakeStoreApi.ts
+++ next-store/constants/fakeStoreApi.ts
@@ -2,7 +2,7 @@ const BASE_URL = 'https://fakestoreapi.com/'
 
 export const PRODUCTS_URL = BASE_URL + 'products'
 export const CATEGORIES_URL = PRODUCTS_URL + '/categories'
-export const CARTS_URL = 'carts'
+export const CARTS_URL = BASE_URL + 'carts'
 export const USERS_URL = BASE_URL + 'users'
 export const LOGIN_URL = BASE_URL + 'auth/login'
 
---
+++ next-store/pages/carts/confirm.tsx
@@ -0,0 +1,84 @@
+import { message, Space } from "antd"
+import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import { addNewOrder, getCategories } from "../../api/fakeStoreApi"
+import CartTable from "../../components/CartTable"
+import CustomerCard from "../../components/CustomerCard"
+import { LS_AUTH_USER, LS_CART } from "../../constants/localStorage"
+import MainLayout from "../../layouts/MainLayout"
+import { UserType } from "../../types/apiResponses"
+import { ProductInCartType } from "../../types/cart"
+import styles from "../../styles/ConfirmOrder.module.scss"
+
+const ConfirmOrder: NextPage = ({ categories }: InferGetServerSidePropsType<GetServerSideProps>) => {
+    const [user, setUser] = useState<UserType | null>(null)
+    const [items, setItems] = useState<Array<ProductInCartType> | null>(null)
+
+    const router = useRouter()
+
+    useEffect(() => {
+        setUser(JSON.parse(localStorage.getItem(LS_AUTH_USER)!))
+        setItems(JSON.parse(localStorage.getItem(LS_CART)!))
+    }, [])
+
+    const handleBack = () => {
+        router.push('/carts/current')
+    }
+
+    const handleConfirm = () => {
+        // Fake Store Api needs user ID for new cart. Order confirmation will work for logged in users only
+        if (user) {
+            const date = new Date().toISOString()
+            const products = items!.map((item: ProductInCartType) => ({ productId: item.id, quantity: item.quantity }))
+            const cart = {
+                userId: user!.id,
+                date,
+                products
+            }
+
+            addNewOrder(cart)
+                .then(resp => {
+                    if (resp.id) {
+                        message.success('Order accepted')
+                        router.push('/user/orders')
+                        localStorage.removeItem(LS_CART)
+                    } else {
+                        message.error('Order error! Please, try again later')
+                    }
+                })
+                .catch(err => message.error(`Order error! ${err}`))
+        } else {
+            message.warning('Only authorized users can make orders. Please, log in')
+        }
+    }
+
+    return (
+        <MainLayout categories={categories} title='Confirm order' description='Order confirmation'>
+            <div className={styles.confirm}>
+                <h1>Confirm order</h1>
+                <div className={styles.wrapper}>
+                    {user && <CustomerCard user={user} />}
+                    {items && <CartTable items={items} />}
+                </div>
+                <div className={styles.buttons}>
+                    <Space>
+                        <button onClick={handleBack}>Back</button>
+                        <button onClick={handleConfirm}>Confirm order</button>
+                    </Space>
+                </div>
+            </div>
+        </MainLayout>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const categories = await getCategories()
+    return {
+        props: {
+            categories,
+        },
+    }
+}
+
+export default ConfirmOrder
