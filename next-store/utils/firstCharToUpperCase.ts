0756311086672fd8eceb4a4f9cff87a4ce96b0d5
DmitrySinyapkin <dsinyapkin@mail.ru>
2022-04-30 15:30:21 +0300

    deploy fixes

--- next-store/utils/firstCharToUpperCase.ts
+++ next-store/utils/firstCharToUpperCase.ts
@@ -1,1 +1,5 @@
-export const firstCharToUpperCase = (str: string | string[] | undefined): string => str[0].toUpperCase() + str.slice(1)
+export const firstCharToUpperCase = (str: string | string[] | undefined): string | undefined => {
+    if (str) {
+        return str[0].toUpperCase() + str.slice(1)
+    }
+}
